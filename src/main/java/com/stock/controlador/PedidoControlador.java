98f601b28aab8698734944cae480326f30f1185a
diego.medus <diego.medus@bizitglibal.com>
2026-04-07 19:14:18 -0300

    pedidos

--- src/main/java/com/stock/controlador/PedidoControlador.java
+++ src/main/java/com/stock/controlador/PedidoControlador.java
@@ -92,17 +92,16 @@ public class PedidoControlador {
         for (int i = 0; i < itemIds.length; i++) {
             Vino vino = vinoService.findById(itemIds[i]);
             if (vino != null) {
-                BigDecimal precioPorCaja = precios[i];
-                BigDecimal precioUnitario = precioPorCaja.divide(
-                        new BigDecimal(vino.getCantVinosxcaja()), 2, java.math.RoundingMode.HALF_UP);
-                int totalBotellas = cantidades[i] * vino.getCantVinosxcaja();
-                BigDecimal subtotal = precioPorCaja.multiply(new BigDecimal(cantidades[i]));
+                BigDecimal precioUnitario = precios[i]; // precio por botella
+                BigDecimal precioCaja = precioUnitario.multiply(new BigDecimal(vino.getCantVinosxcaja()));
+                int cantBotellas = cantidades[i];
+                BigDecimal subtotal = precioUnitario.multiply(new BigDecimal(cantBotellas));
 
                 PedidoDetalle detalle = new PedidoDetalle();
                 detalle.setVino(vino);
                 detalle.setPrecioUnitario(precioUnitario);
-                detalle.setPrecioCaja(precioPorCaja);
-                detalle.setCantidad(totalBotellas);
+                detalle.setPrecioCaja(precioCaja);
+                detalle.setCantidad(cantBotellas);
                 detalle.setSubtotal(subtotal);
                 detalle.setPedido(pedidoAGuardar);
                 pedidoAGuardar.getDetalles().add(detalle);
